a71409ac42ade4c1304257c683adc70051d0548f
Greg <88692986+gforston@users.noreply.github.com>
2024-10-31 16:52:23 -0700

    Completed

--- src/services/authService.js
+++ src/services/authService.js
@@ -24,17 +24,3 @@ export function useAuthentication() {
   return user
 }
 
-import { login, logout, loggedInUserDisplayName } from "../services/authService"
-
-export function SignIn() {
-  return <button onClick={login}>Sign In</button>
-}
-
-export function SignOut() {
-  return (
-    <div>
-      Hello, {loggedInUserDisplayName()}  
-      <button onClick={logout}>Sign Out</button>
-    </div>
-  )
-}
